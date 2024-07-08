42366cba13bf749d7360698f6287ed4c628b4c79
Chaitanya Kundety <chaitanyakun@outlook.com>
2024-07-08 15:03:43 +0200

    refactor script

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -1,20 +1,10 @@
-// Register timeline helper
-Handlebars.registerHelper('odd', function(value, options) {
-    if((value % 2) == 0) {
-        return options.fn(this);
-    } else {
-        return options.inverse(this);
-    }
-});
-
-
-$(document).ready(function() {
-    // Initialize carousel
+function initializeCarousel() {
     $('.carousel').carousel({
         interval: 3000
     });
+}
 
-    // Smooth scrolling for navigation links
+function smoothScrollNav() {
     $('a.nav-link').on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
@@ -26,69 +16,59 @@ $(document).ready(function() {
             });
         }
     });
-    
-    $.getJSON('assets/json/mydata.json', function(data) {
-        // Collect projects into flat list
-        let projects = [];
-        data.experience.forEach(exp => {
-            exp.projects.forEach(proj => {
-                if (proj.include) {
-                    projects.push(proj);
-                }
-            });
-        });
-        data.projects = projects;
-        
-        // Compile template
-        var source = $("body").html();
-        var template = Handlebars.compile(source);
-        var rendered = template(data);
-        $("body").html(rendered);
-        
-        // Create word cloud for skills
-        var skills = [];
-        data.skillset.details.forEach(function(detailedSkills) {
-            detailedSkills.skills.forEach(function(skill) {
-                skills.push(skill);
-            });
-        });
+}
 
-        var wordCounts = {};
-        skills.forEach(function(skill) {
-            wordCounts[skill] = (wordCounts[skill] || 0) + 1;
+function drawSkillsCloud(data) {
+    var skills = [];
+    data.skillset.details.forEach(function(detailedSkills) {
+        detailedSkills.skills.forEach(function(skill) {
+            skills.push(skill);
         });
-
-        var wordArray = Object.keys(wordCounts).map(function(key) {
-            return { text: key, size: wordCounts[key] * 10 };
-        });
-
-        d3.layout.cloud().size([500, 300])
-            .words(wordArray)
-            .padding(5)
-            .rotate(function() { return ~~(Math.random() * 2) * 90; })
-            .font("Impact")
-            .fontSize(function(d) { return d.size; })
-            .on("end", draw)
-            .start();
-    
-        function draw(words) {
-            d3.select("#skillsWordCloud").append("svg")
-                .attr("width", 500)
-                .attr("height", 300)
-                .append("g")
-                .attr("transform", "translate(250,150)")
-                .selectAll("text")
-                .data(words)
-                .enter().append("text")
-                .style("font-size", function(d) { return d.size + "px"; })
-                .style("font-family", "Impact")
-                .style("fill", "#007bff")
-                .attr("text-anchor", "middle")
-                .attr("transform", function(d) {
-                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-                })
-                .text(function(d) { return d.text; });
-        }
     });
+
+    var wordCounts = {};
+    skills.forEach(function(skill) {
+        wordCounts[skill] = (wordCounts[skill] || 0) + 1;
+    });
+
+    var wordArray = Object.keys(wordCounts).map(function(key) {
+        return { text: key, size: wordCounts[key] * 10 };
+    });
+
+    d3.layout.cloud().size([500, 300])
+        .words(wordArray)
+        .padding(5)
+        .rotate(function() { return ~~(Math.random() * 2) * 90; })
+        .font("Impact")
+        .fontSize(function(d) { return d.size; })
+        .on("end", draw)
+        .start();
+
+    function draw(words) {
+        d3.select("#skillsWordCloud").append("svg")
+            .attr("width", 500)
+            .attr("height", 300)
+            .append("g")
+            .attr("transform", "translate(250,150)")
+            .selectAll("text")
+            .data(words)
+            .enter().append("text")
+            .style("font-size", function(d) { return d.size + "px"; })
+            .style("font-family", "Impact")
+            .style("fill", "#007bff")
+            .attr("text-anchor", "middle")
+            .attr("transform", function(d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function(d) { return d.text; });
+    }
+}
+
+$(document).ready(function() {
+    initializeCarousel();
+    
+    smoothScrollNav();
+    
+    $.getJSON('assets/json/mydata.json', drawSkillsCloud);
 });
     
